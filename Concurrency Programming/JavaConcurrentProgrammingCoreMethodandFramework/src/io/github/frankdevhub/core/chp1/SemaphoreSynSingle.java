d9082a6891153deea4a22ddd35e6122969a227b1
franklinfang <frankdevhub@163.com>
2018-02-09 22:18:01 +0800

    use semaphore method to request for the shared resource

--- Concurrency Programming/JavaConcurrentProgrammingCoreMethodandFramework/src/io/github/frankdevhub/core/chp1/SemaphoreSynSingle.java
+++ Concurrency Programming/JavaConcurrentProgrammingCoreMethodandFramework/src/io/github/frankdevhub/core/chp1/SemaphoreSynSingle.java
@@ -1,5 +1,7 @@
 package io.github.frankdevhub.core.chp1;
 
+import java.util.concurrent.Semaphore;
+
 /**   
 * Copyright: Copyright (c) 2018 fangchensheng
 * 
@@ -16,5 +18,33 @@ package io.github.frankdevhub.core.chp1;
 * 2018年2月9日     Administrator           v1.0.0        /
 */
 public class SemaphoreSynSingle {
-
+	static class SemaphoreService implements Runnable{
+        private Semaphore sph = new Semaphore(1); 
+		@Override
+		public void run() {
+ 			try {
+ 				//请求资源
+				sph.acquire();
+				Long mark = System.currentTimeMillis();
+				System.out.println("Current Thread begin:\t"+mark);
+				Thread.sleep(3000);
+				sph.release();
+				System.out.println("Currernt Thread end:\t"+mark);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+ 			
+		}
+         
+		
+	}
+	
+	public static void main(String[] args) {
+		SemaphoreService service = new SemaphoreService();
+		Thread testThread1 = new Thread(service);
+		Thread testThread2 = new Thread(service);
+		testThread1.run();
+		testThread2.run();
+	}
 }
