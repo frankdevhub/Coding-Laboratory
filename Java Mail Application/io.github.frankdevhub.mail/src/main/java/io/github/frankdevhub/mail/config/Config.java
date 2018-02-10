c2bcb7d4c081d073bf0f8977a6ca4a2fc3de0973
franklinfang <frankdevhub@163.com>
2018-02-11 00:06:08 +0800

    config used for junit test

---
+++ Java Mail Application/io.github.frankdevhub.mail/src/main/java/io/github/frankdevhub/mail/config/Config.java
@@ -0,0 +1,27 @@
+package io.github.frankdevhub.mail.config;
+
+/**
+ * @author Administrator
+ *
+ */
+public class Config {
+	/**   
+	* Copyright: Copyright (c) 2018 fangchensheng
+	* 
+	* @ClassName: Config.java
+	* @Description: 
+	*
+	* @version: v1.0.0
+	* @author: Administrator
+	* @date: 2018年2月9日 下午7:07:16 
+	*
+	* Modification History:
+	* Date         Author          Version            Description
+	*--------------------------------------------------------------*
+	* 2018年2月9日     Administrator           v1.0.0      创建
+	*/
+	public static final String TEST_MAIL_CONTEXT_DEFAULT = "TEST MAIL";
+	public static final String TEST_MAIL_SEND_FROM = "frankdevhub@163.com";
+	public static final String TEST_MAIL_SEND_TO = "frankdevhub@163.com";
+	
+}
