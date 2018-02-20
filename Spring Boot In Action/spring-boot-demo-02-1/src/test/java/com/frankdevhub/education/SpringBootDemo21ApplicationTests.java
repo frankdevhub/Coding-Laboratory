66e3e91f5a491dc8b2e9192e8671e806a43ee1fd
franklinfang <frankdevhub@163.com>
2018-02-20 22:07:31 +0800

    start a simple spring boot project

--- Spring Boot In Action/spring-boot-demo-02-1/src/test/java/com/frankdevhub/education/SpringBootDemo21ApplicationTests.java
+++ Spring Boot In Action/spring-boot-demo-02-1/src/test/java/com/frankdevhub/education/SpringBootDemo21ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.roncoo.education;
+package com.frankdevhub.education;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -13,7 +13,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import com.roncoo.education.controller.IndexController;
+import com.frankdevhub.education.controller.IndexController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
