787ce5ff49e5f4bb6ffebe82e26debeacdd961d4
frankdevhub <frankdevhub@gmail.com>
2019-10-28 23:34:25 +0800

    isAlive()方法

---
+++ Concurrency Programming/Java Multi-thread Programming/com-frankdevhub-multi-thread/src/com/frankdevhub/foo/chp1/Chp_1_4_MyThread.java
@@ -0,0 +1,16 @@
+package com.frankdevhub.foo.chp1;
+
+/**
+ * @ClassName: Chp_1_4_MyThread
+ * @author: frankdevhub@gmail.com
+ * @date: 2019年10月28日 下午11:32:58
+ * @description: isAlive()方法
+ * @Copyright: 2019 www.frankdevhub.site Inc. All rights reserved.
+ */
+public class Chp_1_4_MyThread extends Thread {
+
+	@Override
+	public void run() {
+
+	}
+}
