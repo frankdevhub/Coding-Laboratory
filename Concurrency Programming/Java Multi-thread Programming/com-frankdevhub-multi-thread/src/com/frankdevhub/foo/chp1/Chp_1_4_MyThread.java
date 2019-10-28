011a0653db3b3a36a033bfd8a556969c88abd344
frankdevhub <frankdevhub@gmail.com>
2019-10-28 23:46:59 +0800

    isAlive()方法

--- Concurrency Programming/Java Multi-thread Programming/com-frankdevhub-multi-thread/src/com/frankdevhub/foo/chp1/Chp_1_4_MyThread.java
+++ Concurrency Programming/Java Multi-thread Programming/com-frankdevhub-multi-thread/src/com/frankdevhub/foo/chp1/Chp_1_4_MyThread.java
@@ -11,6 +11,29 @@ public class Chp_1_4_MyThread extends Thread {
 
 	@Override
 	public void run() {
+		System.out.println("run=" + this.isAlive());
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("run finish.");
+	}
 
+	// return:
+	// begin==false
+	// begin==false
+	// thread do sleep==false
+	// run=true
+	// run finish.
+
+	public static void main(String[] args) {
+		Chp_1_4_MyThread thread = new Chp_1_4_MyThread();
+		System.out.println("begin==" + thread.isAlive());
+
+		System.out.println("begin==" + thread.isAlive());
+		System.out.println("thread do sleep==" + thread.isAlive());
+
+		thread.start();
 	}
 }
