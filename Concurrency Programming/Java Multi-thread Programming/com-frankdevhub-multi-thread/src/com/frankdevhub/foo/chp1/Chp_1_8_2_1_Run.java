da277c3669bad0d6a6f822ed92f6f579b0e7add0
frankdevhub <frankdevhub@gmail.com>
2019-11-04 15:42:26 +0800

    suspend与resume方法的缺点:独占

---
+++ Concurrency Programming/Java Multi-thread Programming/com-frankdevhub-multi-thread/src/com/frankdevhub/foo/chp1/Chp_1_8_2_1_Run.java
@@ -0,0 +1,35 @@
+package com.frankdevhub.foo.chp1;
+
+/**
+ * @ClassName: Chp_1_8_2_1_Run
+ * @author: frankdevhub@gmail.com
+ * @date: 2019年11月4日 下午3:16:54
+ * @description: suspend与resume方法的缺点:独占
+ * @Copyright: 2019 www.frankdevhub.site Inc. All rights reserved.
+ */
+public class Chp_1_8_2_1_Run {
+
+	protected class MyThread extends Thread {
+		private long i = 0;
+
+		@Override
+		public void run() {
+			while (true) {
+				i++;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		try {
+			Thread thread = new Chp_1_8_2_1_Run().new MyThread();
+			// thread.setDaemon(true); 设置守护线程则不会独占死锁
+			thread.start();
+			Thread.sleep(1000);
+			thread.suspend();
+			System.out.println("main end");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
