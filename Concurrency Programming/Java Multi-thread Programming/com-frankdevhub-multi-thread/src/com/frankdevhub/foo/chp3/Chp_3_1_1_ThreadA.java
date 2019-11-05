6e6f0ae46df572d364d473beaaa29fc78aa11d17
frankdevhub <frankdevhub@gmail.com>
2019-11-05 21:40:52 +0800

    不使用等待/通知机制实现线程间通信

--- Concurrency Programming/Java Multi-thread Programming/com-frankdevhub-multi-thread/src/com/frankdevhub/foo/chp3/Chp_3_1_1_ThreadA.java
+++ Concurrency Programming/Java Multi-thread Programming/com-frankdevhub-multi-thread/src/com/frankdevhub/foo/chp3/Chp_3_1_1_ThreadA.java
@@ -4,7 +4,7 @@ package com.frankdevhub.foo.chp3;
  * @ClassName: Chp_3_1_1_ThreadA
  * @author: frankdevhub@gmail.com
  * @date: 2019年11月5日 下午9:36:24
- * @description: 不使用等待/通知机制实现线程间通信 
+ * @description: 不使用等待/通知机制实现线程间通信
  * @Copyright: 2019 www.frankdevhub.site Inc. All rights reserved.
  */
 public class Chp_3_1_1_ThreadA {
