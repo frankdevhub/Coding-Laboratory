90211daf803ceb7887519bb9b95d9d43bae015bc
frankdevhub <frankdevhub@gmail.com>
2019-11-06 09:23:59 +0800

    不使用等待/通知机制实现线程间通信

--- Concurrency Programming/Java Multi-thread Programming/com-frankdevhub-multi-thread/src/com/frankdevhub/foo/chp3/Chp_3_1_1_ThreadA.java
+++ Concurrency Programming/Java Multi-thread Programming/com-frankdevhub-multi-thread/src/com/frankdevhub/foo/chp3/Chp_3_1_1_ThreadA.java
@@ -7,6 +7,26 @@ package com.frankdevhub.foo.chp3;
  * @description: 不使用等待/通知机制实现线程间通信
  * @Copyright: 2019 www.frankdevhub.site Inc. All rights reserved.
  */
-public class Chp_3_1_1_ThreadA {
+public class Chp_3_1_1_ThreadA extends Thread {
 
+	private Chp_3_1_1_MyList list;
+
+	public Chp_3_1_1_ThreadA(Chp_3_1_1_MyList list) {
+		super();
+		this.list = list;
+	}
+
+	@Override
+	public void run() {
+		super.run();
+		try {
+			for (int i = 0; i < 10; i++) {
+				list.add();
+				System.out.println("list has been added " + (i + 1) + "elements");
+				Thread.sleep(1000);
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 }
