a5f3beb071f77a2d153c6c247bf558efe45982c2
frankdevhub <frankdevhub@gmail.com>
2019-11-10 20:32:20 +0800

    验证ServerSocket的accept()方法具有阻塞特性

---
+++ NIO and Socket Programming/src/com/frankdevhub/foo/chp4/Server.java
@@ -0,0 +1,25 @@
+package com.frankdevhub.foo.chp4;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+/**
+ * @ClassName: Server
+ * @author: frankdevhub@gmail.com
+ * @date: 2019年11月10日 下午8:29:22
+ * @description: 验证ServerSocket的accept()方法具有阻塞特性
+ * @Copyright: 2019 www.frankdevhub.site Inc. All rights reserved.
+ */
+public class Server {
+	public static void main(String[] args) {
+		try {
+			ServerSocket server = new ServerSocket(8090);
+			System.out.println("server acccept startTime=" + System.currentTimeMillis());
+			server.accept();
+
+			System.out.println("server acccept endTime=" + System.currentTimeMillis());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
