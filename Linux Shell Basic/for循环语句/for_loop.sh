41926c0f53eed79d9ce1e70e4753454b1167a31d
frankdevhub <frankdevhub@gmail.com>
2019-11-16 23:27:24 +0800

    shell中的for循环

---
+++ Linux Shell Basic/for循环语句/for_loop.sh
@@ -0,0 +1,19 @@
+
+#!/bin/bash
+
+# Version: V1.0
+# Author: clsn
+# Organization: http://blog.znix.top
+# Created Time : 2019-11-16
+# Description:for loop int list test
+#############################################################
+
+
+for i in 1 2 3 4 5
+do
+
+echo "loop test $i "
+
+done
+
+
