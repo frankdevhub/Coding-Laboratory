dcf38066c04e06462f9a919df8fa0582f332f32b
FangChenSheng <frankdevhub@gmail.com>
2020-03-15 20:28:55 +0800

    伪异步I/O的TimeServerHandlerExecutePool

---
+++ Netty_The Definitive Guide_2nd Edition/NIO入门/basic-nio-starter/src/basic/nio/examples/TimeServerHandlerExecutePool.java
@@ -0,0 +1,20 @@
+package basic.nio.examples;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class TimeServerHandlerExecutePool {
+
+	private ExecutorService executor;
+
+	public TimeServerHandlerExecutePool(int maxPoolSize, int queueSize) {
+		executor = new ThreadPoolExecutor(Runtime.getRuntime().availableProcessors(), maxPoolSize, 120L,
+				TimeUnit.SECONDS, new ArrayBlockingQueue<>(queueSize));
+	}
+
+	public void execute(Runnable task) {
+		executor.execute(task);
+	}
+}
