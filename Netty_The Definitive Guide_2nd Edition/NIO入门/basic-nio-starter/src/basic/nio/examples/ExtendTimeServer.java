0db87d66006f6187637a9706f77564b984d01869
FangChenSheng <frankdevhub@gmail.com>
2020-03-15 22:04:34 +0800

    伪异步I/O创建的TimeServer源码分析

--- Netty_The Definitive Guide_2nd Edition/NIO入门/basic-nio-starter/src/basic/nio/examples/ExtendTimeServer.java
+++ Netty_The Definitive Guide_2nd Edition/NIO入门/basic-nio-starter/src/basic/nio/examples/ExtendTimeServer.java
@@ -1,7 +1,38 @@
 package basic.nio.examples;
 
-public class ExtendTimeServer {
-	public static void main(String[] args) {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class ExtendTimeServer {
+	public static void main(String[] args) throws IOException {
+		int port = 8080;
+		try {
+			if (args != null && args.length > 0) {
+				port = Integer.valueOf(args[0]);
+			}
+		} catch (NumberFormatException e) {
+			// TODO: handle exception
+		}
+		ServerSocket server = null;
+		try {
+			server = new ServerSocket(port);
+			System.out.println("The time server is start in port in : " + port);
+			Socket socket = null;
+			TimeServerHandlerExecutePool singleExecutor = new TimeServerHandlerExecutePool(50, 10000);
+			while (true) {
+				socket = server.accept();
+				singleExecutor.execute(new TimeServerHandler(socket));
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		} finally {
+			if (server != null) {
+				System.out.println("The time server close");
+				server.close();
+				server = null;
+			}
+
+		}
 	}
 }
